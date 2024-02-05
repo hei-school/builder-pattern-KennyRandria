4395bc74d79d79cbb80191942563eb76e8078ba9
KennyRandria <103984550+KennyRandria@users.noreply.github.com>
2024-02-05 18:48:02 +0300

    Update car.js

--- src/car.js
+++ src/car.js
@@ -1,9 +1,10 @@
 class Car {
-    constructor(seats, engine, tripComputer, gps) {
+    constructor(seats, engine, tripComputer, gps, manual) {
       this.seats = seats;
       this.engine = engine;
       this.tripComputer = tripComputer;
       this.gps = gps;
+      this.manual =manual
     }
   }
   
